892f689a09031485732b809d7f8693374117ec16
faizalrazak <mohamadfaizalrazak@gmail.com>
2018-06-01 00:08:04 +0800

    master/detail components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 2, name: 'Mr.Nice' },
+	{ id: 3, name: 'Narco' },
+	{ id: 4, name: 'Bombasto' },
+	{ id: 5, name: 'Celeristas' },
+	{ id: 6, name: 'Magneta' },
+	{ id: 7, name: 'Rubberman'},
+	{ id: 8, name: 'Dynama'},
+	{ id: 9, name: 'Dr IQ'},
+	{ id: 10, name: 'Magma'},
+	{ id: 11, name: 'Tornado'}
+	];
